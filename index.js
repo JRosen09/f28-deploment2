27d6075dad947c150c157200ad3ff3fa0a8c90e5
JRosen09 <2jacobsbusiness@gmail.com>
2022-09-21 16:47:45 -0500

    deployment pt2

---
+++ index.js
@@ -0,0 +1,23 @@
+//setup the server boilerplate
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+//frontend  routes
+app.use(express.json());
+
+//THIS IS MIDDLEWARE TO SEND WHOLE FOLDER
+app.use("/", express.static(path.join(__dirname, "./client/index.html")));
+
+app.use(express.static(path.join(__dirname, "./client")));
+
+//end points
+
+//port range
+app.listen(port, () => {
+  console.log("Docked at port" + port);
+});
