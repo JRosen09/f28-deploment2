240066556385b951ee06da78fc30262069cc0e2e
JRosen09 <2jacobsbusiness@gmail.com>
2022-09-21 17:59:35 -0500

    added css

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
